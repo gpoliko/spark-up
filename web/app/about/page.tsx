a78793102b20b40ba7ba34a7bfcada501d79579d
Giovanni <60165179+gpoliko@users.noreply.github.com>
2023-06-24 23:17:14 +1200

    add causes and about pages

---
+++ web/app/about/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Box, Title } from '@mantine/core';
+
+export default function About() {
+  return (
+    <Box maw={1200} mx='auto'>
+      <Title order={1}>About</Title>
+    </Box>
+  );
+}
